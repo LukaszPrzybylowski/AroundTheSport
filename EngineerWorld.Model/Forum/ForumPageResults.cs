2a82f9feff0673ad93fbd2c8db436a72b165702a
LukaszPrzybylowski <lukasz.przybylowski9@gmail.com>
2023-02-13 09:17:47 +0100

    Initial project commit

---
+++ EngineerWorld.Model/Forum/ForumPageResults.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineerWorld.Model.Forum
+{
+    public class ForumPagedResults<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalCount { get; set; }
+    }
+    
+}
