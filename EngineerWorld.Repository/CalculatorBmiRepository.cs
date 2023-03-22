1a5ec5b2b97e62f1f5d98ec3ec4d872f0869928f
Jakub Przybyłowski <jakub.przybylowski95@gmail.com>
2023-03-22 16:55:38 +0100

    Add CalculatorBmiRepository

---
+++ EngineerWorld.Repository/CalculatorBmiRepository.cs
@@ -0,0 +1,25 @@
+﻿using EngineerWorld.Repository.Calculator;
+using EngineerWorld.Repository.Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineerWorld.Repository
+{
+    public class CalculatorBmiRepository : ICalculatorBmiRepository
+    {
+        private readonly IBmiClassificationDeterminator _bmiClassificationDeterminator;
+        public CalculatorBmiRepository(IBmiClassificationDeterminator bmiClassificationDeterminator)
+        {
+            _bmiClassificationDeterminator = bmiClassificationDeterminator;
+        }
+        public async Task<BmiResult> GetBmiResult(double weight, double height, UnitSystem unitSystem)
+        {
+            var facade = new BmiCalculatorFacade(unitSystem, _bmiClassificationDeterminator);
+
+            return await facade.BmiGetReulst(weight, height);
+        }
+    }
+}
---
+++ EngineerWorld.Repository/ICalculatorBmiRepository.cs
@@ -0,0 +1,14 @@
+﻿using EngineerWorld.Repository.Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineerWorld.Repository
+{
+    public interface ICalculatorBmiRepository
+    {
+        public Task<BmiResult> GetBmiResult(double weight,double height, UnitSystem unitSystem);
+    }
+}
